378da0375af4b6f9a1562db8de52d36a09942431
Pallavi <37789944+Pall104@users.noreply.github.com>
2018-03-31 11:20:33 +0530

    indented code

--- resources/views/ShopifyWeb/flexPanel.blade.php
+++ resources/views/ShopifyWeb/flexPanel.blade.php
@@ -7,35 +7,35 @@
   <link rel="stylesheet" type="text/css" href="css/flex.css">
 </head>
 <body>
-  <div class="panels">
-    <div class="panel panel1">
-      <p>Hey</p>
-      <p>Let's</p>
-      <p>Dance</p>
-    </div>
-    <div class="panel panel2">
-      <p>Give</p>
-      <p>Take</p>
-      <p>Receive</p>
-    </div>
-    <div class="panel panel3">
-      <p>Experience</p>
-      <p>It</p>
-      <p>Today</p>
-    </div>
-    <div class="panel panel4">
-      <p>Give</p>
-      <p>All</p>
-      <p>You can</p>
-    </div>
-    <div class="panel panel5">
-      <p>Life</p>
-      <p>In</p>
-      <p>Motion</p>
-    </div>
+<div class="panels">
+  <div class="panel panel1">
+    <p>Hey</p>
+    <p>Let's</p>
+    <p>Dance</p>
   </div>
+  <div class="panel panel2">
+    <p>Give</p>
+    <p>Take</p>
+    <p>Receive</p>
+  </div>
+  <div class="panel panel3">
+    <p>Experience</p>
+    <p>It</p>
+    <p>Today</p>
+  </div>
+  <div class="panel panel4">
+    <p>Give</p>
+    <p>All</p>
+    <p>You can</p>
+  </div>
+  <div class="panel panel5">
+    <p>Life</p>
+    <p>In</p>
+    <p>Motion</p>
+  </div>
+</div>
 
-  <script>
+<script>
     const panels = document.querySelectorAll('.panel');
 
     function toggleOpen(){
@@ -49,7 +49,7 @@
     }
     panels.forEach(panel => panel.addEventListener('click',toggleOpen));
     panels.forEach(panel => panel.addEventListener('transitionend',toggleActive));
-  </script>
+</script>
 
 </body>
 </html>
